c84fdf8385550198dca09791caf345e9d25544a0
Alan Wostenberg <awostenberg@gmail.com>
2020-11-04 06:47:47 -0700

    r remove duplication

--- src/g4/Console.fs
+++ src/g4/Console.fs
@@ -18,20 +18,15 @@ let run (args:string[]) =
     let usage() = printfn "console usage: --piped <inputfile> --comma <inputfile> --space <inputfile> --orderBy gender|birth|name"
     let args = Seq.toList args
     let rec run' accum args =
-        //printfn "command line args %A" args
-        //todo read in 3 files in [x] --piped, --comma, and --space delimited. Use existing InputFormat parser common to web app"
-        //todo order by --gender, --birth date, or [x] --name. Use existing orderBy* common to web app"
-        //x Display dates in the format M/D/YYYY"
         //todo maybe refactor console.fs towards a separate .exe, sharing common lib of what is in Model..
         //     I take that to be intent of "Within the same code base, build a standalone REST API with the following endpoints"
         
+        let appendFile accum format = List.append accum (readFiles format |> Seq.toList)
+ 
         match args with
-        | "--piped"::filename::rest -> let people = List.append accum (readFiles [Piped filename] |> Seq.toList)
-                                       run' people rest
-        | "--comma"::filename::rest -> let people = List.append accum (readFiles [Comma filename] |> Seq.toList)
-                                       run' people rest
-        | "--space"::filename::rest -> let people = List.append accum (readFiles [Space filename] |> Seq.toList)
-                                       run' people rest
+        | "--piped"::filename::rest -> run' (appendFile accum [Piped filename])  rest
+        | "--comma"::filename::rest -> run' (appendFile accum [Comma filename])  rest
+        | "--space"::filename::rest -> run' (appendFile accum [Space filename])  rest
     
         | "--orderBy"::"name"::rest -> accum |> orderByLastNameDescending |> format |> toConsole "by last name descending"
                                        run' accum rest
@@ -39,8 +34,9 @@ let run (args:string[]) =
                                         run' accum rest    
         | "--orderBy"::"gender"::rest -> accum |> orderByGenderThenLastName |> format |> toConsole "by gender (females first) then last name"
                                          run' accum rest
-        | [] -> ()                     
-        | "help"::_ -> usage() // can't easily do --help because dotnet run consumes that
+        | [] -> ()
+        | "--help"::_ 
+        | "help"::_ -> usage() // alias because /dotnet run/ consumes --help
         | oops::_ -> printfn "Error: unknown at %s" oops
                      usage()
                               
